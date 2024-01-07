8e5ac67265c19547580d67a7019068bb5c473fae
k16diablo <miguel92@naver.com>
2024-01-07 16:25:42 +0900

    minor

--- setup.sh
+++ setup.sh
@@ -1,43 +1,44 @@
-echo "#"
-echo "######################## Build Protobuf #########################################"
-echo "#"
-cd $GEM_FORGE_TOP/lib/protobuf
-./autogen.sh
-CPPFLAGS=-DGOOGLE_PROTOBUF_NO_RTTI \
-    CXXFLAGS=-fPIC \
-    ./configure \
-    --prefix=$GEM_FORGE_TOP/build \
-    --enable-shared=no \
-    --with-zlib=yes
-make -j $CORES
-make install
-# Build python files.
-cd python
-python3 setup.py build
-cd ../../..
-
-echo "#"
-echo "######################## Build GemForge Transforms ##############################"
-echo "#"
-cd transform
-mkdir -p build
-cd build
-cmake ..
-make -j $CORES
-cd ../..
-
-echo "#"
-echo "######################## Build DRAMSim3 for GEM5 ####################################"
-echo "#"
-cd gem5/ext/dramsim3/DRAMsim3
-mkdir -p build 
-cd build
-cmake ..
-make -j $CORES
-cd ../../../../..
+#echo "#"
+#echo "######################## Build Protobuf #########################################"
+#echo "#"
+#cd $GEM_FORGE_TOP/lib/protobuf
+#./autogen.sh
+#CPPFLAGS=-DGOOGLE_PROTOBUF_NO_RTTI \
+#    CXXFLAGS=-fPIC \
+#    ./configure \
+#    --prefix=$GEM_FORGE_TOP/build \
+#    --enable-shared=no \
+#    --with-zlib=yes
+#make -j $CORES
+#make install
+## Build python files.
+#cd python
+#python3 setup.py build
+#cd ../../..
+#
+#echo "#"
+#echo "######################## Build GemForge Transforms ##############################"
+#echo "#"
+#cd transform
+#mkdir -p build
+#cd build
+#cmake ..
+#make -j $CORES
+#cd ../..
+#
+#echo "#"
+#echo "######################## Build DRAMSim3 for GEM5 ####################################"
+#echo "#"
+#cd gem5/ext/dramsim3/DRAMsim3
+#mkdir -p build 
+#cd build
+#cmake ..
+#make -j $CORES
+#cd ../../../../..
 
 echo "#"
 echo "######################## Build GemForge GEM5 ####################################"
 echo "#"
 cd gem5
-/usr/bin/scons -j${CORES} build/X86/gem5.opt --default=X86 PROTOCOL=MESI_Three_Level_Stream 
+/usr/bin/scons -j${CORES} build/X86/gem5.opt --default=X86 PROTOCOL=MESI_Two_Level_Stream 
+#/usr/bin/scons -j${CORES} build/X86/gem5.opt --default=X86 PROTOCOL=MESI_Three_Level_Stream 
